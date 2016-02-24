184c42d5589104fb736470d7cae1c99a47f1f1b7
cpascual <cpascual@users.sourceforge.net>
2016-02-24 23:37:26 +0100

    Bump to v1.7.1

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+import os
 from setuptools import setup, find_packages
 
 
+# entry points to create scripts
 entry_points={
     'console_scripts': [
         'pascual_batch = PAScual.PAScual:start',
@@ -11,17 +13,36 @@ entry_points={
     ]
 }
 
+# classifiers
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: X11 Applications :: Qt",
+    "Environment :: Win32 (MS Windows)",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 2 :: Only",
+    "Topic :: Scientific/Engineering",
+]
+
+# call setup()
 setup(
     name='PAScual',
-    version='1.6',
+    version='1.7.1',
     url='https://sourceforge.net/p/pascual/w/Main_Page/',
     license='GPLv3',
     author='Carlos Pascual-Izarra',
     author_email='cpascual@users.sourceforge.net',
     description='Positron Annihilation Spectroscopy data analysis',
+    long_description=open(os.path.join(os.path.dirname(__file__),
+                                       'README.txt')).read(),
     keywords = "science PALS PAS positron annihilation fit",
 
     packages=find_packages(),
     entry_points=entry_points,
-    install_requires=['numpy'],)
+    install_requires=['numpy'],
+    classifiers=classifiers,
+
+)
 
