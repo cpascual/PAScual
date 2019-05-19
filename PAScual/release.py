31660d1affa2454325e6ac2c3977ef01d167968b
Carlos Pascual <cpascual@cells.es>
2019-05-19 16:07:59 +0200

    Update homepage info and bump version to 2.1.3
    Change homepage from sf.net to github

--- PAScual/release.py
+++ PAScual/release.py
@@ -18,6 +18,6 @@
 """
 # Common metadata to be used by the whole package
 
-__version__ = "2.1.2"
-__homepage__ = "http://pascual.sourceforge.net"
+__version__ = "2.1.3"
+__homepage__ = "https://github.com/cpascual/PAScual"
 __citation_html__ = 'C. Pascual-Izarra et al., <i>Advanced Fitting Algorithms for Analysing Positron Annihilation Lifetime Spectra</i>, Nuclear Instruments and Methods A, 603, p456-466 (2009) <p><a href="http://dx.doi.org/10.1016/j.nima.2009.01.205">(DOI: 10.1016/j.nima.2009.01.205)</a> <p>see %s for up-to-date information about citing</p>' % __homepage__
