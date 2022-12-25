c4de5bdd58b8e84fad6a46641d30d9b29016c3fd
Carlos Pascual <cpascual@cells.es>
2022-12-25 20:23:12 +0100

    bump version to 3.0.1

--- PAScual/release.py
+++ PAScual/release.py
@@ -18,7 +18,7 @@
 """
 # Common metadata to be used by the whole package
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 __homepage__ = "https://github.com/cpascual/PAScual"
 __citation_html__ = (
     'C. Pascual-Izarra et al., <i>Advanced Fitting Algorithms for Analysing Positron Annihilation Lifetime Spectra</i>, Nuclear Instruments and Methods A, 603, p456-466 (2009) <p><a href="http://dx.doi.org/10.1016/j.nima.2009.01.205">(DOI: 10.1016/j.nima.2009.01.205)</a> <p>see %s for up-to-date information about citing</p>'
